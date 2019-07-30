5f88e8e96b526631abc26e4a454edd6c79038539
wallas938 <drame.sissako001@gmail.com>
2019-07-30 10:49:18 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gestionnaire-de-livre';
+
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyAy1UhjNO70sIMlibMV8C-_16Nq483ATJw",
+      authDomain: "bookshelves-ad036.firebaseapp.com",
+      databaseURL: "https://bookshelves-ad036.firebaseio.com",
+      projectId: "bookshelves-ad036",
+      storageBucket: "",
+      messagingSenderId: "591959863427",
+      appId: "1:591959863427:web:32b17e255d86da2a"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
